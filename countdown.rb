399cb4b382946ee7f06a7b7375dbcd04d991031f
Sean Conrad <seanconrad123@gmail.com>
2018-05-31 06:32:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@ def countdown(n)
   counter = n
   for i in 1..n do
     if counter == 1
-      return "HAPPY NEW YEAR!""
+      return "HAPPY NEW YEAR!"
     end
     puts "#{counter} SECOND(S)!"
     counter = counter - 1
