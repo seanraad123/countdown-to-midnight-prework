4c01c8f05c5fcad6d7b46eb50b8a8e49aee19f01
Sean Conrad <seanconrad123@gmail.com>
2018-05-31 06:38:07 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,5 +12,13 @@ def countdown(n)
 end
 
 def countdown_with_sleep(n)
-
+  counter = n
+  for i in 1..n do
+    if counter == 1
+      puts "#{counter} SECOND(S)!"
+      return "HAPPY NEW YEAR!"
+    end
+    puts "#{counter} SECOND(S)!"
+    
+    counter -= 1
 end
