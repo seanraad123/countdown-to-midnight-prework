59ce0ca87691cf3134b031499e6198c2c766b8db
Sean Conrad <seanconrad123@gmail.com>
2018-05-31 06:39:50 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
-#write your code here
+def countdown(n)
+  counter = n
+  for i in 1..n do
+    if counter == 1
+      puts "#{counter} SECOND(S)!"
+      return "HAPPY NEW YEAR!"
+    end
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
 
-def countdown
+  end
+end
+
+def countdown_with_sleep(n)
+  counter = n
+  for i in 1..n do
+    if counter == 1
+      puts "#{counter} SECOND(S)!"
+      sleep(1)
+      return "HAPPY NEW YEAR!"
+    end
+    puts "#{counter} SECOND(S)!"
+    sleep(1)
+    counter -= 1
+  end
 end
